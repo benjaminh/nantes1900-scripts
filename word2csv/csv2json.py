f463d673bd19fb2df4371768723bc8de37ab43c2
benjamin.hervy@irccyn.ec-nantes.fr <bhervy@bhervy-PORTEGE-ubuntu.(none)>
2013-05-23 22:25:45 +0200

    Résolution bug dû aux accents dans json

--- word2csv/csv2json.py
+++ word2csv/csv2json.py
@@ -6,7 +6,7 @@ import csv
 import json
 
 f = open( 'sample.csv', 'r' )
-reader = csv.DictReader( f, fieldnames = ( "Latitude","Longitude","Titre","Résumé" ) )
+reader = csv.DictReader( f, fieldnames = ( "Latitude","Longitude","Titre","Resume" ) )
 out = json.dumps( [ row for row in reader ] )
 coord_json = open('coord.json','a')
 coord_json.write(out)
